8d995918f59634b31a5973809eea13cb8087ed00
Ali Tabesh <ali.tabesh76@gmail.com>
2021-09-18 16:46:51 +0430

    FindAll query added + some improvement

--- src/handler.js
+++ src/handler.js
@@ -1,30 +1,73 @@
 "use strict"
 
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
+const jwt = require("jsonwebtoken");
 var apiMateDB;  // Cached connection-pool for further requests.
+const resheaders = {
+  "Access-Control-Allow-Origin": "http://localhost:3000"
+};
 
 module.exports = async (event, context) => {
-  return prepareDB().then(async (db) => {
-    let ret = await HandleRequest(db, event.body);
-    if (ret == null)
-      return context
-        .status(404)
-        .succeed("Not found");
-    else if (ret == "Query not found")
-      return context
-        .status(400)
-        .succeed(ret);
-    else
-      return context
-        .status(200)
-        .headers({
-          "Content-type": "application/json; charset=utf-8"
-        })
-        .succeed(ret)
-  })
-    .catch(err => {
-      return context.fail(err.toString());
-    });
+  if (event.method === 'OPTIONS') {
+    var headers = {};
+    headers["Access-Control-Allow-Origin"] = "http://localhost:3000";
+    headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, OPTIONS";
+    headers["Access-Control-Allow-Credentials"] = false;
+    headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+    headers["Access-Control-Allow-Headers"] = "*";
+
+    return context
+      .status(200)
+      .headers(headers)
+      .succeed();
+  } else {
+    return prepareDB().then(async (db) => {
+      console.log(event);
+      let req = event.body;
+      if (event.headers["authorization"] && event.headers["authorization"].startsWith("Bearer ") && event.headers["authorization"].length > 7 && req.query == null) {
+        console.log("vfret");
+        const vfret = verifyToken(event.headers["authorization"].split(' ')[1]);
+        console.log(vfret);
+        if (vfret.user_id) {
+          req = {
+            record: vfret.user_id,
+            query: "findById",
+            table: "Users",
+          }
+        }
+        else return context.headers(resheaders).fail(vfret.toString());
+      }
+      console.log(req);
+      let ret = await HandleRequest(db, req);
+      if (ret == null)
+        return context
+          .status(404)
+          .headers(resheaders)
+          .succeed("Not found");
+      else if (ret == "Query not found")
+        return context
+          .status(400)
+          .headers(resheaders)
+          .succeed(ret);
+      else {
+        //if (req.table == "Users") ret.password = null;
+        return context
+          .status(200)
+          .headers({
+            "Content-type": "application/json; charset=utf-8",
+            "Access-Control-Allow-Origin": "http://localhost:3000"
+          })
+          .succeed(ret)
+      }
+    })
+      .catch(err => {
+        return context
+          .status(500)
+          .headers(resheaders)
+          .succeed(err.toString());
+      });
+  }
 }
 
 async function HandleRequest(db, req) {
@@ -40,14 +83,20 @@ async function HandleRequest(db, req) {
       case "insertMany":
         ret = db.collection(table).insertMany(record);
         break;
-      case "findOne":
-        ret = db.collection(table).findOne(record);
+      case "findById":
+        ret = db.collection(table).findOne({ _id: new ObjectId(record) });
+        break;
+      case "findAll":
+        ret = db.collection(table).find({}).toArray();
         break;
       case "find":
         ret = db.collection(table).find(record);
         break;
       case "updateOne":
         ret = db.collection(table).updateOne(record);
+        break;
+      case "findOne":
+        ret = db.collection(table).findOne(record);
         break;
       // case "updateMany":
       //   ret = db.collection(table).updateMany(record);
@@ -64,6 +113,20 @@ async function HandleRequest(db, req) {
     resolve(ret);
   });
 }
+
+const verifyToken = (token) => {
+  // const token =
+  //   req.body.token || req.query.token || req.headers["x-access-token"];
+  if (!token) {
+    return "A token is required for authentication";
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_TOKEN_KEY);
+    return decoded;
+  } catch (err) {
+    return "Invalid Token " + err.toString();
+  }
+};
 
 const prepareDB = () => {
   const url = process.env.mongoconnect;
